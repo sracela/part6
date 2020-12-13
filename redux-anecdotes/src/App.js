960faac10e7f7ee9899ded8165aa580502a28944
sracela <saracelalfonso@gmail.com>
2020-12-14 00:09:11 +0100

    6.1, 6.2 , 6.3: anecdotes, step1

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { voteTo } from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -7,6 +8,7 @@ const App = () => {
 
   const vote = (id) => {
     console.log('vote', id)
+    dispatch(voteTo(id))
   }
 
   return (
